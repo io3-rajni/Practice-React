827d1de6e9d9148f3a119d4208f9a4fc1c80fc73
rajni <io3.rajni@gamil.com>
2023-08-11 00:12:11 +0530

    footer dailog box open

--- practiceapp/src/Componant/Footer.js
+++ practiceapp/src/Componant/Footer.js
@@ -1,59 +1,55 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import SiginDailog from "./SiginDailog";
 
 const Footer = () => {
-    const handleLogin = () => {
-        console.log("Login")
-    }
-    return (
-        <div>
-            <Box sx={{ flexGrow: 1 }}>
-                <AppBar position="static">
-                    <Toolbar>
-                        <IconButton
-                            size="large"
-                            edge="start"
-                            color="inherit"
-                            aria-label="menu"
-                            sx={{ mr: 2 }}
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Typography>
+  const [footer, setFooter] = React.useState(false);
+  const handleLogin = () => {
+    setFooter(true);
+    console.log("Login");
+  };
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography>
+              <Box sx={{ display: { xs: "none", md: "inline" } }}>
+                <Button sx={{ my: 2, color: "white" }}>Contact</Button>
+              </Box>
+              <Box sx={{ display: { xs: "none", md: "inline" } }}>
+                <Button sx={{ my: 2, color: "white" }}>Help</Button>
+              </Box>{" "}
+              <Box sx={{ display: { xs: "none", md: "inline" } }}>
+                <Button sx={{ my: 2, color: "white" }}>Complaint</Button>
+              </Box>
+              <Box sx={{ display: { xs: "none", md: "inline" } }}>
+                <Button onClick={handleLogin} sx={{ my: 2, color: "white" }}>
+                  Login
+                </Button>
+              </Box>
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <SiginDailog footer={footer} setFooter={setFooter} />
+    </div>
+  );
+};
 
-                            <Box sx={{ display: { xs: 'none', md: 'inline' } }}>
-
-                                <Button sx={{ my: 2, color: 'white' }}>
-                                    Contact</Button>
-                            </Box>
-                            <Box sx={{ display: { xs: 'none', md: 'inline' } }}>
-
-                                <Button sx={{ my: 2, color: 'white' }}>
-                                    Help</Button>
-                            </Box> <Box sx={{ display: { xs: 'none', md: 'inline' } }}>
-
-                                <Button sx={{ my: 2, color: 'white' }}>
-                                    Complaint</Button>
-                            </Box>
-                            <Box sx={{ display: { xs: 'none', md: 'inline' } }}>
-
-                                <Button onClick={handleLogin} sx={{ my: 2, color: 'white' }}>
-                                    Login</Button>
-                            </Box>
-                        </Typography>
-
-                    </Toolbar>
-                </AppBar>
-            </Box>
-
-        </div>
-    )
-}
-
-export default Footer
+export default Footer;
--- practiceapp/src/Componant/SiginDailog.js
+++ practiceapp/src/Componant/SiginDailog.js
@@ -1,108 +1,85 @@
-import React from 'react'
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { TextField, Box } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
+import React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { TextField, Box } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 const SiginDailog = (props) => {
-    const { sigin, setSigin } = props
+  const { sigin, setSigin, footer, setFooter } = props;
 
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
 
+  // const handleClickOpen = () => {
+  //     setOpen(true);
+  // };
 
-    const theme = useTheme();
-    const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
-
-    // const handleClickOpen = () => {
-    //     setOpen(true);
-    // };
-
-    const handleClose = () => {
-        setSigin(false);
-    };
-
-    return (
-        <div>
-            {/* <Button variant="outlined" onClick={handleClickOpen}>
+  const handleClose = () => {
+    setSigin(false);
+  };
+  const handleCloseFooter = () => {
+    setFooter(false);
+    console.log("footer");
+  };
+  return (
+    <div>
+      {/* <Button variant="outlined" onClick={handleClickOpen}>
                 Open responsive dialog
             </Button> */}
-            <Dialog
-                fullScreen={fullScreen}
-                open={sigin}
-                onClose={handleClose}
-                aria-labelledby="responsive-dialog-title"
+      <Dialog
+        fullScreen={fullScreen}
+        open={(sigin, footer)}
+        onClose={handleClose}
+        onCloseFooter={handleCloseFooter}
+        aria-labelledby="responsive-dialog-title"
+      >
+        <DialogTitle id="responsive-dialog-title">
+          {"Enter Your Details"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            <Box
+              component="form"
+              sx={{
+                "& .MuiTextField-root": { m: 1, width: "25ch" },
+              }}
+              noValidate
+              autoComplete="off"
             >
-                <DialogTitle id="responsive-dialog-title">
-                    {"Enter Your Details"}
-                </DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        <Box
-                            component="form"
-                            sx={{
-                                '& .MuiTextField-root': { m: 1, width: '25ch' },
-                            }}
-                            noValidate
-                            autoComplete="off"
-                        >
-                            <div>
-                                <TextField
-                                    placeholder='First Name'
-                                    type='text'
-                                />
-                                <TextField
-
-                                    placeholder='Last Name'
-                                    type='text'
-
-                                />
-                            </div>
-                            <div>
-                                <TextField
-
-                                    placeholder='DOB'
-                                    type='date'
-                                />
-                                <TextField
-
-                                    placeholder='Quualification'
-                                    type='text'
-                                />
-                            </div>
-                            <div>
-                                <TextField
-
-                                    placeholder='State'
-                                    type='text'
-                                />
-                                <TextField
-                                    placeholder='Pincode'
-                                    type="text"
-                                    inputProps={{ maxlength: 6 }}
-                                />
-                            </div>
-                        </Box>
-
-
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button autoFocus onClick={handleClose}>
-
-                        Cancle
-                    </Button>
-                    {/* <Button onClick={} autoFocus>
+              <div>
+                <TextField placeholder="First Name" type="text" />
+                <TextField placeholder="Last Name" type="text" />
+              </div>
+              <div>
+                <TextField placeholder="DOB" type="date" />
+                <TextField placeholder="Quualification" type="text" />
+              </div>
+              <div>
+                <TextField placeholder="State" type="text" />
+                <TextField
+                  placeholder="Pincode"
+                  type="text"
+                  inputProps={{ maxlength: 6 }}
+                />
+              </div>
+            </Box>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose}>
+            Cancle
+          </Button>
+          {/* <Button onClick={} autoFocus>
                         Agree
                     </Button> */}
-                </DialogActions>
-            </Dialog>
-        </div>
-    );
-}
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
 
-
-
-export default SiginDailog
+export default SiginDailog;
