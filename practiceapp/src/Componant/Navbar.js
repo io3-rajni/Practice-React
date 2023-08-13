9af61d5caea3cc930e65d3264cb717a149f7f8e2
sapna maurya <io5.sapnamaurya@gmail.com>
2023-08-14 00:39:08 +0530

    profile 'page

--- practiceapp/src/Componant/Navbar.js
+++ practiceapp/src/Componant/Navbar.js
@@ -1,155 +1,154 @@
-import React, { useState } from 'react'
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+import React, { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
 import Test from "./Test";
 
-import ProfileDailog from './ProfileDailog'
-import SiginDailog from './SiginDailog'
+import ProfileDailog from "./ProfileDailog";
+import SiginDailog from "./SiginDailog";
 const Navbar = () => {
-    const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
-    const [anchorElNav, setAnchorElNav] = React.useState(null);
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
-    const [siginOpen, setSiginOpen] = React.useState(false);
-    const [profileOpen, setProfileOpen] = useState(false);
-    const [testOpen, setTestOpen] = React.useState(false);
+  const settings = ["Profile", "Account", "Dashboard", "Logout"];
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [siginOpen, setSiginOpen] = React.useState(false);
+  const [profileOpen, setProfileOpen] = useState(false);
+  const [testOpen, setTestOpen] = React.useState(false);
 
-    const handleOpenNavMenu = (event) => {
-        setAnchorElNav(event.currentTarget);
-    };
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
-    };
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
-    const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
-    };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-    };
-    const handleSignIn = () => {
-        setSiginOpen(true)
-        console.log("sigin")
-    }
-    const handleProfile = () => {
-        setProfileOpen(true)
-        console.log("Profile")
-    }
-    const handleTest = () => {
-        setTestOpen(true)
-        console.log("Test")
-    }
-    const handleChild = (data) => {
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+  const handleSignIn = () => {
+    setSiginOpen(true);
+    console.log("sigin");
+  };
+  const handleProfile = () => {
+    setProfileOpen(true);
+    console.log("Profile");
+  };
+  const handleTest = () => {
+    setTestOpen(true);
+    console.log("Test");
+  };
+  const handleChild = (data) => {
+    console.log("child tarnfer", data);
+  };
+  const handleChildProfile = (profileData) => {
+    console.log("Profile", profileData);
+  };
+  return (
+    <div>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              LOGO
+            </Typography>
 
-        console.log("child tarnfer", data)
-    }
-    return (
-        <div>
+            <Box sx={{ display: { xs: "none", md: "inline" } }}>
+              <Button
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white" }}
+              >
+                Home{" "}
+              </Button>
+            </Box>
+            <Box sx={{ display: { xs: "none", md: "inline" } }}>
+              <Button onClick={handleSignIn} sx={{ my: 2, color: "white" }}>
+                Sign In{" "}
+              </Button>
+            </Box>
+            <Box sx={{ display: { xs: "none", md: "inline" } }}>
+              <Button onClick={handleProfile} sx={{ my: 2, color: "white" }}>
+                Profile{" "}
+              </Button>
+            </Box>
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "inline" } }}>
+              <Button onClick={handleTest} sx={{ my: 2, color: "white" }}>
+                Test
+              </Button>
+            </Box>
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
 
-            <AppBar position="static">
-                <Container maxWidth="xl">
-                    <Toolbar disableGutters>
-                        <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-                        <Typography
-                            variant="h6"
-                            noWrap
-                            component="a"
-                            href="/"
-                            sx={{
-                                mr: 2,
-                                display: { xs: 'none', md: 'flex' },
-                                fontFamily: 'monospace',
-                                fontWeight: 700,
-                                letterSpacing: '.3rem',
-                                color: 'inherit',
-                                textDecoration: 'none',
-                            }}
-                        >
-                            LOGO
-                        </Typography>
+      <ProfileDailog
+        profile={profileOpen}
+        setProfile={setProfileOpen}
+        handleChildProfile={handleChildProfile}
+      />
+      <SiginDailog
+        sigin={siginOpen}
+        setSigin={setSiginOpen}
+        handleChild={handleChild}
+      />
+      <Test test={testOpen} testFalse={setTestOpen} />
+    </div>
+  );
+};
 
-
-                        <Box sx={{ display: { xs: 'none', md: 'inline' } }}>
-
-                            <Button onClick={handleCloseNavMenu} sx={{ my: 2, color: 'white', }}>
-                                Home </Button>
-
-                        </Box>
-                        <Box sx={{ display: { xs: 'none', md: 'inline' } }}>
-
-                            <Button onClick={handleSignIn} sx={{ my: 2, color: 'white', }}>
-                                Sign In </Button>
-
-                        </Box>
-                        <Box sx={{ display: { xs: 'none', md: 'inline' } }}>
-
-                            <Button onClick={handleProfile} sx={{ my: 2, color: 'white' }}>
-                                Profile </Button>
-                        </Box>
-                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'inline' } }}>
-
-                            <Button onClick={handleTest} sx={{ my: 2, color: 'white' }}>
-                                Test</Button>
-                        </Box>
-                        <Box sx={{ flexGrow: 0 }}>
-                            <Tooltip title="Open settings">
-                                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                    <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                                </IconButton>
-                            </Tooltip>
-                            <Menu
-                                sx={{ mt: '45px' }}
-                                id="menu-appbar"
-                                anchorEl={anchorElUser}
-                                anchorOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                open={Boolean(anchorElUser)}
-                                onClose={handleCloseUserMenu}
-                            >
-                                {settings.map((setting) => (
-                                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                                        <Typography textAlign="center">{setting}</Typography>
-                                    </MenuItem>
-                                ))}
-                            </Menu>
-                        </Box>
-                    </Toolbar>
-                </Container>
-
-            </AppBar>
-
-            <ProfileDailog
-                profile={profileOpen}
-                setProfile={setProfileOpen}
-            />
-            <SiginDailog
-                sigin={siginOpen}
-                setSigin={setSiginOpen}
-                handleChild={handleChild}
-            />
-            <Test
-                test={testOpen}
-                testFalse={setTestOpen} />
-        </div >
-    )
-}
-
-export default Navbar
+export default Navbar;
--- practiceapp/src/Componant/ProfileDailog.js
+++ practiceapp/src/Componant/ProfileDailog.js
@@ -1,152 +1,212 @@
-import React, { useState } from 'react'
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { TextField, Box } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { TextField, Box } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 
 const ProfileDailog = (props) => {
-    const [open, setOpen] = useState(false)
-    const theme = useTheme();
-    const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
-    const { profile, setProfile } = props
+  const { profile, setProfile, handleChildProfile } = props;
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
+  const [pic, setPic] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [middleName, setMiddleName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [pin, setPin] = useState("");
+  const [date, setDate] = useState("");
+  const [qualification, setQualification] = useState("");
+  const [number, setNumber] = useState("");
+  const [address, setAddress] = useState("");
+  const [email, setEmail] = useState("");
+  const [bio, setBio] = useState("");
+  //   const [data, setData] = useState({
+  //     Photo: "",
+  //     FirstName: "",
+  //     MiddleName: "",
+  //     LastName: "",
+  //     Age: "",
+  //     Qualification: "",
+  //     State: "",
+  //     Pincode: "",
+  //     Number: "",
+  //     Email: "",
+  //     Address: "",
+  //     bio: "",
+  //   });
 
+  const handleClose = () => {
+    setProfile(false);
+  };
+  const handlePic = (e) => {
+    setPic(e?.target?.value);
+  };
+  const handleFirstName = (e) => {
+    setFirstName(e?.target?.value);
+  };
+  const handleMiddleName = (e) => {
+    setMiddleName(e?.target?.value);
+  };
+  const handleLastName = (e) => {
+    setLastName(e?.target?.value);
+  };
+  const handleQualification = (e) => {
+    setQualification(e?.target?.value);
+  };
+  const handleDate = (e) => {
+    setDate(e?.target?.value);
+  };
+  const handleNumber = (e) => {
+    setNumber(e?.target?.value);
+  };
+  const handleEmail = (e) => {
+    setEmail(e?.target?.value);
+  };
+  const handleBio = (e) => {
+    setBio(e?.target?.value);
+  };
+  const handleAddress = (e) => {
+    setAddress(e?.target?.value);
+  };
+  const handlePincode = (e) => {
+    setPin(e?.target?.value);
+  };
+  const handleSubmit = () => {
+    handleChildProfile({
+      Photo: pic,
+      FirstName: firstName,
+      MiddleName: middleName,
+      LastName: lastName,
+      Age: date,
+      Qualification: qualification,
+      State: "",
+      Pincode: pin,
+      Number: number,
+      Email: email,
+      Address: address,
+      bio: bio,
+    });
+  };
 
-    const handleClose = () => {
-        setProfile(false);
-    };
-    return (
+  return (
+    <>
+      <Dialog
+        fullScreen={fullScreen}
+        open={profile}
+        onClose={handleClose}
+        aria-labelledby="responsive-dialog-title"
+      >
+        <DialogTitle id="responsive-dialog-title">
+          {"Enter Your Details"}
+        </DialogTitle>
+        <DialogContent>
+          <Box
+            component="form"
+            sx={{
+              "& .MuiTextField-root": { m: 1, width: "25ch" },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <>
+              <TextField
+                placeholder="Paste Photo"
+                type="file"
+                onChange={handlePic}
+                value={pic}
+              />
 
-        <>
+              <>
+                <TextField
+                  placeholder="First Name"
+                  type="text"
+                  onChange={handleFirstName}
+                  value={firstName}
+                />
+                <TextField
+                  placeholder="Middle Name"
+                  type="text"
+                  onChange={handleMiddleName}
+                  value={middleName}
+                />
+                <TextField
+                  placeholder="Last Name"
+                  type="text"
+                  onChange={handleLastName}
+                  value={lastName}
+                />
+                <TextField
+                  placeholder="DOB"
+                  type="date"
+                  onChange={handleDate}
+                  value={date}
+                />
+              </>
+              <>
+                <TextField placeholder="Qualification" type="text" />
+                <TextField
+                  placeholder="Bio"
+                  type="text"
+                  inputProps={{ maxLength: 50 }}
+                  onChange={handleBio}
+                  value={bio}
+                />
+              </>
+              <TextField
+                placeholder="Number"
+                type="text"
+                inputProps={{ maxLength: 12 }}
+                onChange={handleNumber}
+                value={number}
+              />
+              <TextField
+                placeholder="Email"
+                type="text"
+                onChange={handleEmail}
+                value={email}
+              />
+            </>
+            <>
+              <TextField
+                placeholder="Qualification"
+                type="text"
+                onChange={handleQualification}
+                value={qualification}
+              />
+              <TextField
+                placeholder="Address"
+                type="text"
+                onChange={handleAddress}
+                value={address}
+              />
+            </>
+            <>
+              <TextField placeholder="State" type="text" />
+              <TextField
+                placeholder="Pincode"
+                type="text"
+                inputProps={{ maxLength: 6 }}
+                onChange={handlePincode}
+                value={pin}
+              />
+            </>
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose}>
+            Cancle
+          </Button>
+          <Button onClick={handleSubmit} autoFocus>
+            Submit
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
 
-            <Dialog
-                fullScreen={fullScreen}
-                open={profile}
-                onClose={handleClose}
-                aria-labelledby="responsive-dialog-title"
-            >
-                <DialogTitle id="responsive-dialog-title">
-                    {"Enter Your Details"}
-                </DialogTitle>
-                <DialogContent>
-
-                    <Box
-                        component="form"
-                        sx={{
-                            '& .MuiTextField-root': { m: 1, width: '25ch' },
-                        }}
-                        noValidate
-                        autoComplete="off"
-                    >
-                        <>
-                            <TextField
-                                placeholder='Paste Photo'
-                                type='file'
-                            />
-
-                            <>
-                                <TextField
-                                    placeholder='First Name'
-                                    type='text'
-                                />
-                                <TextField
-                                    placeholder='Middle Name'
-                                    type='text'
-                                />
-                                <TextField
-
-                                    placeholder='Last Name'
-                                    type='text'
-
-                                />
-                                <TextField
-
-                                    placeholder='DOB'
-                                    type='date'
-                                />
-
-                            </>
-                            <>
-                                <TextField
-
-                                    placeholder='Qualification'
-                                    type='text'
-                                />
-                                <TextField
-
-                                    placeholder='Bio'
-                                    type='text'
-                                    inputProps={{ maxlength: 50 }}
-                                />
-
-
-
-                            </>
-                            <TextField
-
-                                placeholder='Number'
-                                type='text'
-                                inputProps={{ maxlength: 12 }}
-                            />
-                            <TextField
-
-                                placeholder='Email'
-                                type='text'
-
-                            />
-
-
-                        </>
-                        <>
-
-
-
-                            <TextField
-
-                                placeholder='Qualification'
-                                type='text'
-                            />
-                            <TextField
-
-                                placeholder='Address'
-                                type='text'
-
-                            />
-                        </>
-                        <>
-                            <TextField
-
-                                placeholder='State'
-                                type='text'
-                            />
-                            <TextField
-
-                                placeholder='Pincode'
-                                type='text'
-                                inputProps={{ maxlength: 6 }}
-                            />
-
-                        </>
-                    </Box>
-
-
-
-                </DialogContent>
-                <DialogActions>
-                    <Button autoFocus onClick={handleClose}>
-                        Cancle
-                    </Button>
-                    {/* <Button onClick={} autoFocus>
-                        Agree
-                    </Button> */}
-                </DialogActions>
-            </Dialog>
-        </>
-    )
-}
-
-export default ProfileDailog
+export default ProfileDailog;
